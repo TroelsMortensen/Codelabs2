dde44a8ebac575da41c4da71df200a324826ac2c
TRMO <tromortensen@hotmail.com>
2025-05-27 21:26:22 +0200

    trying rerouting from home page

--- UI/Pages/Home.razor.cs
+++ UI/Pages/Home.razor.cs
@@ -10,10 +10,22 @@ public partial class Home : ComponentBase
     [Inject] public NavigationManager NavMgr { get; set; }
     [Inject] public ArticlesState ArticlesState { get; set; }
 
+    [Parameter] public string? TutorialsName { get; set; }
+    [Parameter] public string? Owner { get; set; }
+
     private List<ArticleHeader>? articles;
 
-    protected override async Task OnInitializedAsync() =>
-        articles = await ArticlesState.GetArticleHeaders();
+    protected override async Task OnInitializedAsync()
+    {
+        if (!string.IsNullOrEmpty(TutorialsName) && !string.IsNullOrEmpty(Owner))
+        {
+            NavigateToArticle(Owner, TutorialsName);
+        }
+        else
+        {
+            articles = await ArticlesState.GetArticleHeaders();
+        }
+    }
 
     private void NavigateToArticle(string owner, string tutorialName) =>
         NavMgr.NavigateTo($"article/{owner}/{tutorialName}");
