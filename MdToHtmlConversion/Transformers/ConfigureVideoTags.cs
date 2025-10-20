a393cf4d009bf89785e814cb1bdd52cf5a644418
Troels Mortensen <tromortensen@hotmail.com>
2025-10-20 13:40:12 +0200

    refactor video tag handler

--- MdToHtmlConversion/Transformers/ConfigureVideoTags.cs
+++ MdToHtmlConversion/Transformers/ConfigureVideoTags.cs
@@ -4,48 +4,36 @@ namespace MdToHtmlConversion.Transformers;
 
 public class ConfigureVideoTags : ITransformer
 {
-    public string Handle(string html, string articleName)
-    {
-        return VideoToIframe(html);
-    }
+    public string Handle(string html, string articleName) =>
+        VideoToIframe(html);
 
-    public static string VideoToIframe(string inputHtml)
-    {
-        string result = ReplaceDirectLinks(inputHtml);
-        result = ReplaceQueryLinks(result);
-        return result;
-    }
+    public static string VideoToIframe(string inputHtml) =>
+        inputHtml
+            .ReplaceDirectLinks()
+            .ReplaceQueryLinks();
+}
 
-    private static string ReplaceDirectLinks(string inputHtml)
-    {
-        string pattern = @"<p><video\s+src=""https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)\""\s*><\/video><\/p>";
+public static class StringExtensions
+{
+    private const string Replacement = """
+                                       <div class="video-box">
+                                       <p>
+                                       <iframe src="https://youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                       </p>
+                                       </div>
+                                       """;
 
-        string replacement = @"
-<div class=""video-box"">
-<p>
-<iframe src=""https://youtube.com/embed/$1"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"" allowfullscreen></iframe>
-</p>
-</div>";
+    public static string ReplaceDirectLinks(this string inputHtml) =>
+        Regex.Replace(
+            inputHtml,
+            pattern: """<p><video\s+src="https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)\"\s*><\/video><\/p>""",
+            Replacement
+        );
 
-        string result = Regex.Replace(inputHtml, pattern, replacement);
-        return result;
-    }
-
-    // <video src="https://www.youtube.com/watch?v=Qb_NUn0TSAU"></video>
-    
-    private static string ReplaceQueryLinks(string inputHtml)
-    {
-        string pattern = @"<p><video\s+src=""https:\/\/www.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)\""\s*><\/video><\/p>";
-        
-        
-        string replacement = @"
-<div class=""video-box"">
-<p>
-<iframe src=""https://youtube.com/embed/$1"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"" allowfullscreen></iframe>
-</p>
-</div>";
-
-        string result = Regex.Replace(inputHtml, pattern, replacement);
-        return result;
-    }
+    public static string ReplaceQueryLinks(this string inputHtml) =>
+        Regex.Replace(
+            inputHtml,
+            pattern: """<p><video\s+src="https:\/\/www.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)\"\s*><\/video><\/p>""",
+            Replacement
+        );
 }
