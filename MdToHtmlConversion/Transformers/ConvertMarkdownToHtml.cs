05010eb041c603cefac759d54e3f80a2e3ba5446
Troels Mortensen <tromortensen@hotmail.com>
2025-10-20 13:40:24 +0200

    refactor md to html converter

--- MdToHtmlConversion/Transformers/ConvertMarkdownToHtml.cs
+++ MdToHtmlConversion/Transformers/ConvertMarkdownToHtml.cs
@@ -4,13 +4,12 @@ namespace MdToHtmlConversion.Transformers;
 
 public class ConvertMarkdownToHtml : ITransformer
 {
-    public string Handle(string markdown, string articleName)
-    {
-        var pipeline = new MarkdownPipelineBuilder()
-            .UseAdvancedExtensions()
-            .Build();
-        
-        string html = Markdown.ToHtml(markdown, pipeline);
-        return html;
-    }
+    public string Handle(string markdown, string articleName) =>
+        Markdown
+            .ToHtml(
+                markdown,
+                pipeline: new MarkdownPipelineBuilder()
+                    .UseAdvancedExtensions()
+                    .Build()
+            );
 }
