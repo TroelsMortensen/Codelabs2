7f6242dda18e0f5c66b8cfac82d5d13fbfc45533
Troels Mortensen <tromortensen@hotmail.com>
2025-10-20 13:27:47 +0200

    Youtube video links using query arg format should now be transformed correctly from md to html

--- MdToHtmlConversion/Transformers/ConfigureVideoTags.cs
+++ MdToHtmlConversion/Transformers/ConfigureVideoTags.cs
@@ -11,6 +11,13 @@ public class ConfigureVideoTags : ITransformer
 
     public static string VideoToIframe(string inputHtml)
     {
+        string result = ReplaceDirectLinks(inputHtml);
+        result = ReplaceQueryLinks(result);
+        return result;
+    }
+
+    private static string ReplaceDirectLinks(string inputHtml)
+    {
         string pattern = @"<p><video\s+src=""https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)\""\s*><\/video><\/p>";
 
         string replacement = @"
@@ -21,7 +28,24 @@ public class ConfigureVideoTags : ITransformer
 </div>";
 
         string result = Regex.Replace(inputHtml, pattern, replacement);
+        return result;
+    }
 
+    // <video src="https://www.youtube.com/watch?v=Qb_NUn0TSAU"></video>
+    
+    private static string ReplaceQueryLinks(string inputHtml)
+    {
+        string pattern = @"<p><video\s+src=""https:\/\/www.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)\""\s*><\/video><\/p>";
+        
+        
+        string replacement = @"
+<div class=""video-box"">
+<p>
+<iframe src=""https://youtube.com/embed/$1"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"" allowfullscreen></iframe>
+</p>
+</div>";
+
+        string result = Regex.Replace(inputHtml, pattern, replacement);
         return result;
     }
 }
---
+++ Tests/HtmlTransformers/ConfigureVideoTagsTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+using Markdig;
+using MdToHtmlConversion.Transformers;
+
+namespace Tests.HtmlTransformers;
+
+public class ConfigureVideoTagsTests
+{
+    [Fact]
+    public void HtmlWithCorrectlyFormattedTag_ShouldProduceCorrectVideoTag()
+    {
+        string markdown = """
+                          ## Summary
+
+                          The `super` keyword is essential for inheritance in Java:
+
+                          1. **`super()`** - Calls super class constructor (must be first in constructor)
+                          2. **`super.method()`** - Calls parent class method
+                          3. **`super.field`** - Accesses parent class field
+                          4. **Always refers to immediate super class** in inheritance chain
+
+                          Understanding `super` is crucial for building proper inheritance hierarchies and maintaining the relationship between super class and subclass.
+
+
+                          ## Video
+
+                          John explains the `super` keyword in 11 minutes, if you need to learn about it in a different way:
+
+                          <video src="https://youtu.be/Qb_NUn0TSAU"></video>
+
+                          You may optionally watch this video.
+                          """;
+        var pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .Build();
+        
+        string html = Markdown.ToHtml(markdown, pipeline);
+        string result = new ConfigureVideoTags().Handle(html, "");
+        Assert.Contains("https://youtube.com/embed/Qb_NUn0TSAU", result);
+    }
+    
+    [Fact]
+    public void HtmlWithCorrectlyFormattedQueryTag_ShouldProduceCorrectVideoTag()
+    {
+        string markdown = """
+                          ## Summary
+
+                          The `super` keyword is essential for inheritance in Java:
+
+                          1. **`super()`** - Calls super class constructor (must be first in constructor)
+                          2. **`super.method()`** - Calls parent class method
+                          3. **`super.field`** - Accesses parent class field
+                          4. **Always refers to immediate super class** in inheritance chain
+
+                          Understanding `super` is crucial for building proper inheritance hierarchies and maintaining the relationship between super class and subclass.
+
+
+                          ## Video
+
+                          John explains the `super` keyword in 11 minutes, if you need to learn about it in a different way:
+
+                          <video src="https://www.youtube.com/watch?v=Qb_NUn0TSAU"></video>
+
+                          You may optionally watch this video.
+                          """;
+        var pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .Build();
+        
+        string html = Markdown.ToHtml(markdown, pipeline);
+        string result = new ConfigureVideoTags().Handle(html, "");
+        Assert.Contains("https://youtube.com/embed/Qb_NUn0TSAU", result);
+    }
+
+    [Fact]
+    public void TestRegExMatch()
+    {
+        string pattern = @"<video\s+src=""https:\/\/www.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)\""\s*><\/video>";
+        Match match = Regex.Match("<video src=\"https://www.youtube.com/watch?v=Qb_NUn0TSAU\"></video>", pattern);
+        ;
+    }
+    
+    
+}
