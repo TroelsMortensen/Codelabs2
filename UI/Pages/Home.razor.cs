7c222e139f5ad3100013bf33eb64538da6d973ba
Troels Mortensen <tromortensen@hotmail.com>
2025-10-24 11:12:04 +0200

    structure and data format sort of works.
    Still need to apply styling. And move data to some json file, I can pull from github, to make it easier to update stuff.

--- UI/Pages/Home.razor.cs
+++ UI/Pages/Home.razor.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
-using UI.Data.Models;
-using UI.State;
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Components;
 
 namespace UI.Pages;
 
@@ -8,16 +7,61 @@ public partial class Home : ComponentBase
 {
     // [Inject] public HttpClient Client { get; set; }
     [Inject] public NavigationManager NavMgr { get; set; }
-    [Inject] public ArticlesState ArticlesState { get; set; }
+    // [Inject] public ArticlesState ArticlesState { get; set; }
 
 
-    private List<ArticleHeader>? articles;
+    // private List<ArticleHeader>? articles;
 
-    protected override async Task OnInitializedAsync()
-    {
-        articles = await ArticlesState.GetArticleHeaders();
-    }
+    // protected override async Task OnInitializedAsync()
+    // {
+    //     articles = await ArticlesState.GetArticleHeaders();
+    // }
 
     private void NavigateToArticle(string owner, string tutorialName) =>
         NavMgr.NavigateTo($"article/{owner}/{tutorialName}");
+
+    private CourseOverview OverviewData { get; } =
+        new CourseOverview(
+            [
+                new Course("PRO1", Color.Aquamarine.ToArgb().ToString(),
+                    [
+                        new Session(1, "Basic Java",
+                            [
+                                new LearningPath("Introduction", "Session%201%20Learning%20Path")
+                            ]
+                        ),
+                        new Session(2, "Input",
+                            [
+                                new LearningPath("Default Values", "Session%202%20Default%20Values"),
+                                new LearningPath("String methods", "Session%202%20String%20methods"),
+                                new LearningPath("Console input", "Session%202%20Console%20Input"),
+                                new LearningPath("Extra exercises", "Session%202%20-%20live"),
+                            ]
+                        )
+                    ]
+                ),
+                new Course("UML", Color.Coral.ToArgb().ToString(),
+                    [
+                        new Session(1, "Class Diagram",
+                            [
+                                new LearningPath("Documentation", "UML%20Class%20Diagrams")
+                            ]
+                        ),
+                        new Session(2, "Domain Model",
+                            [
+                                new LearningPath("Documentation", "UML%20Domain%20Model")
+                            ]
+                        )
+                    ]
+                )
+            ]
+        );
 }
+
+record CourseOverview(IEnumerable<Course> Courses);
+
+record Course(string Title, string Color, IEnumerable<Session> Sessions);
+
+record Session(int SessionNumber, String Title, IEnumerable<LearningPath> LearningPaths);
+
+record LearningPath(String Title, String Url);
