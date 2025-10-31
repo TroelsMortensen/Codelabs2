135d3d2320bd58a75ea36538aec2f66133b175f9
Troels Mortensen <tromortensen@hotmail.com>
2025-10-31 15:01:04 +0100

    working on automatically generating the front page layout. Maybe not all that important..?

--- FrontPageStructure/Program.cs
+++ FrontPageStructure/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string pathToArticles = @"C:\MyStuff\DotNet Projects\Codelabs2\Articles";
+string pathToStructureJson = @"C:\MyStuff\DotNet Projects\Codelabs2\Articles/front-page-structure.json";
+
+IEnumerable<string> enumerateDirectories = Directory.EnumerateDirectories(pathToArticles);
+
+List<Folder> folders = enumerateDirectories
+    .Select(dir => new Folder(dir.Substring(dir.LastIndexOf('\\')+1), []))
+    .ToList();
+
+folders.ForEach(Console.WriteLine);
+
+PopulateWithSubFolders(folders);
+
+void PopulateWithSubFolders(List<Folder> folders)
+{
+    
+}
+
+record Folder(string Name, List<Folder> SubFolders);
