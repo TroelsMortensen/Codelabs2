49376ff38500a52b82398ff36265991f0135d106
Troels Mortensen <tromortensen@hotmail.com>
2025-12-18 21:43:27 +0100

    link

--- UI/Pages/Home.razor.cs
+++ UI/Pages/Home.razor.cs
@@ -215,6 +215,7 @@ public partial class Home : ComponentBase
                             [
                                 new LearningPath("Keys in the domain model", "SDT/Keys in domains"),
                                 new LearningPath("Design Patterns", "SDT/Design Patterns"),
+                                new LearningPath("Design Patterns", "SDT/Design Principles"),
                                 new LearningPath("Data Transfer Objects", "SDT/DTOs"),
                                 new LearningPath("Records",  "SDT/Records"),
                             ]
