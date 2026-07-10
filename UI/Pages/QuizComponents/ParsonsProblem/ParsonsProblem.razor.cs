c533f8fc77984bd04273a61e1c1feda322f64492
Troels Mortensen <tromortensen@hotmail.com>
2026-07-10 20:29:01 +0200

    fixed with a tiny bit of js

--- UI/Pages/QuizComponents/ParsonsProblem/ParsonsProblem.razor.cs
+++ UI/Pages/QuizComponents/ParsonsProblem/ParsonsProblem.razor.cs
@@ -7,7 +7,7 @@ namespace UI.Pages.QuizComponents.ParsonsProblem;
 public partial class ParsonsProblem : ComponentBase
 {
     [Parameter, EditorRequired] public ParsonsProblemSegment Data { get; set; } = null!;
-    [Inject] public IJSRuntime JS { get; set; } = null!;
+    [Inject] private IJSRuntime JS { get; set; } = null!;
 
     private ParsonsProblemSegment? _initializedData;
     private List<ParsonsProblemLine> _orderedLines = [];
@@ -16,6 +16,8 @@ public partial class ParsonsProblem : ComponentBase
     private bool _isChecked;
     private bool _allCorrect;
     private int _correctCount;
+    private int? _hintLineIndex;
+    private int? _hintSlotIndex;
 
     protected override void OnParametersSet()
     {
@@ -29,6 +31,37 @@ public partial class ParsonsProblem : ComponentBase
         _isChecked = false;
         _allCorrect = false;
         _correctCount = 0;
+        ClearDragState();
+    }
+
+    private async Task OnDragEnd()
+    {
+        ClearDragState();
+        await JS.InvokeVoidAsync("ppDrag.clearVisuals");
+    }
+
+    private void OnLineDragOver(int index)
+    {
+        if (_isChecked || _hintLineIndex == index)
+            return;
+
+        _hintLineIndex = index;
+        _hintSlotIndex = null;
+    }
+
+    private void OnSlotDragOver(int slotIndex)
+    {
+        if (_isChecked || _hintSlotIndex == slotIndex)
+            return;
+
+        _hintSlotIndex = slotIndex;
+        _hintLineIndex = null;
+    }
+
+    private void ClearDragState()
+    {
+        _hintLineIndex = null;
+        _hintSlotIndex = null;
     }
 
     private Task OnLineDrop(int lineIndex) => OnDrop(lineIndex, fromSlot: false);
@@ -40,17 +73,19 @@ public partial class ParsonsProblem : ComponentBase
         if (_isChecked)
             return;
 
-        var sourceIndex = await JS.InvokeAsync<int?>("parsonsDrag.consumeSourceIndex");
-        if (sourceIndex is null || sourceIndex == insertIndex)
+        var sourceIndex = await JS.InvokeAsync<int?>("ppDrag.consumeIndex");
+        if (sourceIndex is not int source || source == insertIndex)
             return;
 
-        var item = _orderedLines[sourceIndex.Value];
-        _orderedLines.RemoveAt(sourceIndex.Value);
+        var item = _orderedLines[source];
+        _orderedLines.RemoveAt(source);
 
-        if (fromSlot && sourceIndex.Value < insertIndex)
+        if (fromSlot && source < insertIndex)
             insertIndex--;
 
         _orderedLines.Insert(insertIndex, item);
+        ClearDragState();
+        await JS.InvokeVoidAsync("ppDrag.clearVisuals");
     }
 
     private void CheckAnswer()
@@ -65,12 +100,34 @@ public partial class ParsonsProblem : ComponentBase
         _allCorrect = _correctCount == _correctOrder.Count;
     }
 
-    private async Task TryAgain()
+    private void TryAgain()
     {
         _isChecked = false;
         _allCorrect = false;
         _correctCount = 0;
-        await JS.InvokeVoidAsync("parsonsDrag.clearAllVisuals");
+        ClearDragState();
+    }
+
+    private string GetSlotHintClass(int slotIndex) =>
+        _hintSlotIndex == slotIndex ? "pp-drop-slot-active" : string.Empty;
+
+    private string GetLineDragClass(int index)
+    {
+        var classes = new List<string>();
+
+        var resultClass = GetLineCssClass(index);
+        if (!string.IsNullOrEmpty(resultClass))
+            classes.Add(resultClass);
+
+        if (_hintLineIndex == index)
+            classes.Add("pp-line-drop-active");
+
+        if ((_hintLineIndex == index && index == 0) || _hintSlotIndex == index)
+            classes.Add("pp-line-shift-down");
+        else if ((_hintLineIndex == index && index > 0) || _hintSlotIndex == index + 1)
+            classes.Add("pp-line-shift-up");
+
+        return string.Join(' ', classes);
     }
 
     private string GetLineCssClass(int index)
