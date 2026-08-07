aacea0860062ccecbad20e1e7eda2126ab24f6c7
Troels Mortensen <tromortensen@hotmail.com>
2026-08-07 13:24:03 +0200

    moved all pro1 learning paths to folder

--- UI/Pages/Home.razor.cs
+++ UI/Pages/Home.razor.cs
@@ -14,145 +14,145 @@ public partial class Home : ComponentBase
                     [
                         new Session(1, "Basic Java",
                             [
-                                new LearningPath("Introduction", "Session 1 Learning Path"),
-                                new LearningPath("Default Values", "Session 2 Default Values"),
+                                new LearningPath("Introduction", "PRO1/Session 1 Learning Path"),
+                                new LearningPath("Default Values", "PRO1/Session 2 Default Values"),
                             ]
                         ),
                         new Session(2, "Input",
                             [
-                                new LearningPath("String methods", "Session 2 String methods"),
-                                new LearningPath("Console input", "Session 2 Console Input"),
-                                new LearningPath("The Java Compiler", "The Java Compiler"),
-                                new LearningPath("IntelliJ shortcuts", "IntelliJ Shortcuts 1"),
-                                new LearningPath("Extra exercises", "Session 2 - live"),
+                                new LearningPath("String methods", "PRO1/Session 2 String methods"),
+                                new LearningPath("Console input", "PRO1/Session 2 Console Input"),
+                                new LearningPath("The Java Compiler", "PRO1/The Java Compiler"),
+                                new LearningPath("IntelliJ shortcuts", "PRO1/IntelliJ Shortcuts 1"),
+                                new LearningPath("Extra exercises", "PRO1/Session 2 - live"),
                             ]
                         ),
                         new Session(3, "If-statement",
                             [
-                                new LearningPath("Boolean logic recap", "Session 3 Boolean logic"),
-                                new LearningPath("The if-statement", "Session 3 If statement"),
-                                new LearningPath("Extra exercises", "Session 3 - live"),
+                                new LearningPath("Boolean logic recap", "PRO1/Session 3 Boolean logic"),
+                                new LearningPath("The if-statement", "PRO1/Session 3 If statement"),
+                                new LearningPath("Extra exercises", "PRO1/Session 3 - live"),
                             ]
                         ),
                         new Session(4, "Switch-statement",
                             [
                                 new("Enums", "Session 4 Enums"),
-                                new("The switch statement", "Session 4 Switch statement")
+                                new("The switch statement", "PRO1/Session 4 Switch statement")
                             ]
                         ),
                         new Session(5, "Loops",
                             [
-                                new("Increment and decrement", "Session 5 Inc and Dec"),
-                                new("For-loop", "Session 5 For loop"),
-                                new("While-loop", "Session 5 While loop"),
+                                new("Increment and decrement", "PRO1/Session 5 Inc and Dec"),
+                                new("For-loop", "PRO1/Session 5 For loop"),
+                                new("While-loop", "PRO1/Session 5 While loop"),
                             ]
                         ),
                         new Session(6, "Arrays",
                             [
-                                new("Arrays", "Session 6 Arrays")
+                                new("Arrays", "PRO1/Session 6 Arrays")
                             ]
                         ),
                         new Session(7, "Lists",
                             [
-                                new("ArrayList", "Session 7 List")
+                                new("ArrayList", "PRO1/Session 7 List")
                             ]
                         ),
                         new Session(8, "Introducing Objects",
                             [
-                                new("Objects", "Session 8 Objects"),
-                                new("UML", "Session 8 UML"),
-                                new("Encapsulation", "Session 8 Encapsulation"),
-                                new("Shadowing", "Session 8 Shadowing"),
+                                new("Objects", "PRO1/Session 8 Objects"),
+                                new("UML", "PRO1/Session 8 UML"),
+                                new("Encapsulation", "PRO1/Session 8 Encapsulation"),
+                                new("Shadowing", "PRO1/Session 8 Shadowing"),
                             ]
                         ),
                         new Session(9, "More about Objects",
                             [
-                                new("Inheriting from Object", "Session 9 Inherit from Object"),
-                                new("Final keyword", "Session 9 Final fields"),
-                                new("Static keyword", "Session 9 Static"),
-                                new("Overloading methods", "Session 9 Method overloading"),
-                                new("Extra exercises", "Session 9 - Extra exercises")
+                                new("Inheriting from Object", "PRO1/Session 9 Inherit from Object"),
+                                new("Final keyword", "PRO1/Session 9 Final fields"),
+                                new("Static keyword", "PRO1/Session 9 Static"),
+                                new("Overloading methods", "PRO1/Session 9 Method overloading"),
+                                new("Extra exercises", "PRO1/Session 9 - Extra exercises")
                             ]
                         ),
                         new Session(10, "Object exercises",
                             [
-                                new("Garbage collection in Java", "Session 10 - Garbage collection"),
-                                new("Extra exercises", "Session 10 - Exercises")
+                                new("Garbage collection in Java", "PRO1/Session 10 - Garbage collection"),
+                                new("Extra exercises", "PRO1/Session 10 - Exercises")
                             ]
                         ),
                         new Session(11, "Relationships part 1",
                             [
-                                new("Relations between objects", "Session 11 - Relationships intro")
+                                new("Relations between objects", "PRO1/Session 11 - Relationships intro")
                             ]
                         ),
                         new Session(12, "Relationships part 2",
                             [
-                                new("One to many relationships", "Session 12 - One to Many")
+                                new("One to many relationships", "PRO1/Session 12 - One to Many")
                             ]
                         ),
                         new Session(13, "Inheritance part 1",
                             [
-                                new("Introducing Inheritance", "Session 13 - Inheritance")
+                                new("Introducing Inheritance", "PRO1/Session 13 - Inheritance")
                             ]
                         ),
                         new Session(14, "Abstract classes",
                             [
-                                new("Abstract keyword", "Session 14 - Abstract"),
-                                new("Polymorphism", "Session 14 - Polymorphism")
+                                new("Abstract keyword", "PRO1/Session 14 - Abstract"),
+                                new("Polymorphism", "PRO1/Session 14 - Polymorphism")
                             ]
                         ),
                         new Session(15, "Interfaces",
                             [
-                                new("Packages", "Session 15 - Packages"),
-                                new("Interfaces", "Session 15 - Interfaces")
+                                new("Packages", "PRO1/Session 15 - Packages"),
+                                new("Interfaces", "PRO1/Session 15 - Interfaces")
                             ]
                         ),
                         new Session(16, "Exercises",
                             [
-                                new("Extra exercises", "Session 16 - Test")
+                                new("Extra exercises", "PRO1/Session 16 - Test")
                             ]
                         ),
                         new Session(17, "Exceptions",
                             [
-                                new("Exceptions", "Session 17 - Exceptions")
+                                new("Exceptions", "PRO1/Session 17 - Exceptions")
                             ]
                         ),
                         new Session(18, "Robocode",
                             [
-                                new("Robocode documentation", "Robocode")
+                                new("Robocode documentation", "PRO1/Robocode") // TODO move to independent page
                             ]
                         ),
                         new Session(19, "Introducing files",
                             [
-                                new("File I/O", "Session 19 - File IO"),
+                                new("File I/O", "PRO1/Session 19 - File IO"),
                             ]
                         ),
                         new Session(20, "Application design",
                             [
-                                new("Java documentation", "Java Doc"),
-                                new("Automated testing", "Automated testing"),
-                                new("Application design", "Session 20 - Application design")
+                                new("Java documentation", "PRO1/Java Doc"),
+                                new("Automated testing", "PRO1/Automated testing"),
+                                new("Application design", "PRO1/Session 20 - Application design")
                             ]
                         ),
                         new Session(21, "JavaFX introduction",
                             [
-                                new("Lambdas and method references", "Lambda expressions"),
-                                new("Java FX introduction", "Session 21 - JFX intro")
+                                new("Lambdas and method references", "PRO1/Lambda expressions"),
+                                new("Java FX introduction", "PRO1/Session 21 - JFX intro")
                             ]
                         ),
                         new Session(22, "JavaFX - multiple views",
                             [
-                                new("JavaFX - Multiple views", "Session 22 - JFX Continued")
+                                new("JavaFX - Multiple views", "PRO1/Session 22 - JFX Continued")
                             ]
                         ),
                         new Session(23, "JavaFX - Application",
                             [
-                                new("JavaFX - application design", "Session 23 - JFX Application")
+                                new("JavaFX - application design", "PRO1/Session 23 - JFX Application")
                             ]
                         ),
                         new Session(24, "Exam",
                             [
-                                new("Exam practice exercises", "Session 24 - Exam")
+                                new("Exam practice exercises", "PRO1/Session 24 - Exam")
                             ]
                         )
                     ]
